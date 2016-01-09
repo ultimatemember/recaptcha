3c5c0f9b81b21eada3a99d011af0196b71bf0246
jonfalcon <darkutubuki143@gmail.com>
2016-01-08 16:52:58 -0800

    Fix recaptcha not loaded for login modal

--- core/actions/um-recaptcha-form.php
+++ core/actions/um-recaptcha-form.php
@@ -8,49 +8,66 @@
 	function um_recaptcha_add_captcha($args){
 		global $um_recaptcha, $ultimatemember;
 		if ( !$um_recaptcha->captcha_allowed( $args ) ) return;
-		
+
 		$your_sitekey = um_get_option('g_recaptcha_sitekey');
-		
+		$id = 'gcaptch' . rand(0, 100) . microtime();
+
 		?>
-		
-		<div class="g-recaptcha" data-sitekey="<?php echo $your_sitekey; ?>"></div>
-		
+
+		<div id="<?php echo $id; ?>" class="g-recaptcha"></div>
+		<script>
+		var renderCaptcha = function() {
+			grecaptcha.render('<?php echo $id; ?>', {
+    			sitekey: '<?php echo $your_sitekey; ?>'
+    		});
+		}
+		if(typeof grecaptcha === "undefined") {
+			jQuery(window).load(function() {
+				grecaptcha.render('<?php echo $id; ?>', {
+	    			sitekey: '<?php echo $your_sitekey; ?>'
+	    		});
+			});
+		} else {
+			renderCaptcha();
+		}
+		</script>
+
 		<?php
-		
+
 		if ($ultimatemember->form->has_error('recaptcha')) {
 			echo '<div class="um-field-error">' . $ultimatemember->form->errors['recaptcha'] . '</div>';
 		}
-		
+
 	}
-	
+
 	/***
 	***	@form error handling
 	***/
 	add_action('um_submit_form_errors_hook', 'um_recaptcha_validate', 20);
 	function um_recaptcha_validate( $args ){
 		global $um_recaptcha, $ultimatemember;
-		
+
 		if ( isset($args['mode']) && !in_array( $args['mode'], array('login','register') ) ) return;
-		
+
 		if ( !$um_recaptcha->captcha_allowed( $args ) ) return;
-		
+
 		$your_secret = um_get_option('g_recaptcha_secretkey');
 		$client_captcha_response = $_POST['g-recaptcha-response'];
 		$user_ip = $_SERVER['REMOTE_ADDR'];
-		
+
 		$response = wp_remote_get("https://www.google.com/recaptcha/api/siteverify?secret=$your_secret&response=$client_captcha_response&remoteip=$user_ip");
 		if( is_array($response) ) {
-			
+
 			$result = json_decode( $response['body'] );
-			
+
 			if ( ! $result->success ) {
 				$ultimatemember->form->add_error('recaptcha', __('Please confirm you are not a robot','um-recaptcha') );
 			}
-			
+
 		} else {
-			
+
 			$ultimatemember->form->add_error('recaptcha', __('Please confirm you are not a robot','um-recaptcha') );
-		
+
 		}
-	
+
 	}
