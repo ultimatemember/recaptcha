d94fc91512118ff92ec05b078c1098e6dfe17b5e
Champ Camba <heychampsupertramp@gmail.com>
2017-03-01 17:46:46 +0800

    Fix recaptcha response notices

--- core/actions/um-recaptcha-form.php
+++ core/actions/um-recaptcha-form.php
@@ -45,23 +45,34 @@
 
 		if ( !$um_recaptcha->captcha_allowed( $args ) ) return;
 
-		$your_secret = um_get_option('g_recaptcha_secretkey');
+		$your_secret = trim( um_get_option('g_recaptcha_secretkey') );
 		$client_captcha_response = $_POST['g-recaptcha-response'];
 		$user_ip = $_SERVER['REMOTE_ADDR'];
 
 		$response = wp_remote_get("https://www.google.com/recaptcha/api/siteverify?secret=$your_secret&response=$client_captcha_response&remoteip=$user_ip");
-		if( is_array($response) ) {
+
+		$error_codes = array(
+			'missing-input-secret' => __('The secret parameter is missing.','um-recaptcha'),
+			'invalid-input-secret' => __('The secret parameter is invalid or malformed.','um-recaptcha'),
+			'missing-input-response' => __('The response parameter is missing.','um-recaptcha'),
+			'invalid-input-response' => __('The response parameter is invalid or malformed.','um-recaptcha'),
+		);
+
+		if( is_array( $response ) ) {
 
 			$result = json_decode( $response['body'] );
 
-			if ( ! $result->success ) {
-				$ultimatemember->form->add_error('recaptcha', __('Please confirm you are not a robot','um-recaptcha') );
+			if( isset( $result->{'error-codes'} ) && ! $result->success ){
+				foreach( $result->{'error-codes'} as $key => $error_code ){
+
+					if(  $error_code == 'missing-input-response' ){
+						$ultimatemember->form->add_error('recaptcha', __('Please confirm you are not a robot','um-recaptcha') );
+					}else{ 
+						$ultimatemember->form->add_error('recaptcha', $error_codes[ $error_code ] ,'um-recaptcha' );
+					}
+				}
 			}
 
-		} else {
-
-			$ultimatemember->form->add_error('recaptcha', __('Please confirm you are not a robot','um-recaptcha') );
-
-		}
+		} 
 
 	}
