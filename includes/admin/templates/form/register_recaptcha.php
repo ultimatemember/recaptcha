8c1a40605987e40cdf244773f2385cfde4738cbb
nikitozzzzzzz <nsinelnikov.work@gmail.com>
2017-07-20 16:30:19 +0300

    - UM2.0 metaboxes;

--- includes/admin/templates/form/register_recaptcha.php
+++ includes/admin/templates/form/register_recaptcha.php
@@ -1,24 +1,33 @@
 <div class="um-admin-metabox">
 
-	<?php $status = um_get_option('g_recaptcha_status'); ?>
-	
-	<?php if ( $status ) { ?>
-	
-	<p><?php _e('Google reCAPTCHA seems to be <strong style="color:#7ACF58">enabled</strong> by default.','um-recaptcha'); ?></p>
-	
+	<?php $status = um_get_option( 'g_recaptcha_status' );
+
+	if ( $status ) { ?>
+
+		<p><?php _e( 'Google reCAPTCHA seems to be <strong style="color:#7ACF58">enabled</strong> by default.', 'um-recaptcha' ); ?></p>
+
 	<?php } else { ?>
-	
-	<p><?php _e('Google reCAPTCHA seems to be <strong style="color:#C74A4A">disabled</strong> by default.','um-recaptcha'); ?></p>
-	
-	<?php } ?>
-	
-	<p>
-		<label><strong><?php _e('reCAPTCHA status on this form','um-recaptcha'); ?></strong></label>
-		<span>
-			
-			<?php $this->ui_on_off('_um_register_g_recaptcha_status', $status, 0, 0, 0, 0, __('On','um-recaptcha'), __('Off','um-recaptcha') ); ?>
-				
-		</span>
-	</p><div class="um-admin-clear"></div>
-	
+
+		<p><?php _e( 'Google reCAPTCHA seems to be <strong style="color:#C74A4A">disabled</strong> by default.', 'um-recaptcha' ); ?></p>
+
+	<?php }
+
+	UM()->admin_forms( array(
+		'class'		=> 'um-form-register-recaptcha um-top-label',
+		'prefix_id'	=> 'form',
+		'fields' => array(
+			array(
+				'id'		    => '_um_register_g_recaptcha_status',
+				'type'		    => 'select',
+				'label'		    => __( 'reCAPTCHA status on this form', 'um-recaptcha' ),
+				'value'		    => UM()->query()->get_meta_value( '_um_register_g_recaptcha_status', null, $status ),
+				'options'		=> array(
+					'0'	=> __('No', 'um-recaptcha'),
+					'1'	=> __('Yes', 'um-recaptcha')
+				),
+			)
+		)
+	) )->render_form(); ?>
+
+	<div class="um-admin-clear"></div>
 </div>
