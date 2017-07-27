19a9d24b3d8f97c7967ecc4d642462f276dfaf66
Denis Baranov <dbaranov.work@gmail.com>
2017-07-27 15:20:03 +0300

    - fixed initialization of the google recaptcha library - fixed initialization two captcha v2 in single page

--- includes/core/actions/um-recaptcha-form.php
+++ includes/core/actions/um-recaptcha-form.php
@@ -1,110 +1,129 @@
 <?php
 
 	/***
-	***	@add recaptcha
-	***/
-	add_action('um_after_register_fields', 'um_recaptcha_add_captcha', 500);
-	add_action('um_after_login_fields', 'um_recaptcha_add_captcha', 500);
-	function um_recaptcha_add_captcha($args){
-		if ( ! UM()->reCAPTCHA_API()->captcha_allowed( $args ) ) return;
+	 ***    @add recaptcha
+	 ***/
+	add_action( 'um_after_register_fields', 'um_recaptcha_add_captcha', 500 );
+	add_action( 'um_after_login_fields', 'um_recaptcha_add_captcha', 500 );
+	add_action( 'wp_footer', 'um_recaptcha_google_onload' );
+	function um_recaptcha_add_captcha( $args )
+	{
+		if (!UM()->reCAPTCHA_API()->captcha_allowed( $args )) return;
 
-		$your_sitekey = um_get_option('g_recaptcha_sitekey');
-		$language_code = um_get_option('g_recaptcha_language_code');
+		$your_sitekey = um_get_option( 'g_recaptcha_sitekey' );
 
 		$options = array(
-			'data-type' => um_get_option('g_recaptcha_type'),
-			'data-size' => um_get_option('g_recaptcha_size'),
-			'data-theme' => um_get_option('g_recaptcha_theme'),
-			
+			'data-type'  => um_get_option( 'g_recaptcha_type' ),
+			'data-size'  => um_get_option( 'g_recaptcha_size' ),
+			'data-theme' => um_get_option( 'g_recaptcha_theme' ),
+
 		);
 
-		
+
 		?>
 
-		<?php if( 'invisible' == $options['data-size'] ): ?>
-		<script>
-		 
-		   var onSubmit = function(token) {
-		   		var me = jQuery('.um-<?php _e( $args['form_id'] );?> form');
-		   		me.attr('disabled','disabled');
-	         	me.submit();
-	        };
+		<?php if ('invisible' == $options['data-size']): ?>
+        <script>
 
-	        var onloadCallback = function() {
-	          grecaptcha.render('um-submit-btn', {
-	            'sitekey' : '<?php _e($your_sitekey);?>',
-	            'callback' : onSubmit
-	          });
-	        };
+            var onSubmit = function (token) {
+                var me = jQuery('.um-<?php _e( $args['form_id'] );?> form');
+                me.attr('disabled', 'disabled');
+                me.submit();
+            };
 
-	        jQuery(document).ready(function(){
-	        	jQuery('.um-<?php _e( $args['form_id'] );?> #um-submit-btn').addClass('um-has-recaptcha');
-	        });
-		  
-		</script>
-		<script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit&hl=<?php _e($language_code);?>" async defer></script>
-		
-		<?php else: ?>
-			<?php $options['data-sitekey']  =  $your_sitekey; ?>
-			<script src="https://www.google.com/recaptcha/api.js?hl=<?php _e($language_code);?>" async defer></script>
-		<?php endif; ?>
+            var onloadCallback = function () {
+                grecaptcha.render('um-submit-btn', {
+                    'sitekey': '<?php _e( $your_sitekey );?>',
+                    'callback': onSubmit
+                });
+            };
 
-		<?php 
+            jQuery(document).ready(function () {
+                jQuery('.um-<?php _e( $args['form_id'] );?> #um-submit-btn').addClass('um-has-recaptcha');
+            });
+
+        </script>
+
+	<?php else: ?>
+		<?php $options['data-sitekey'] = $your_sitekey; ?>
+	<?php endif; ?>
+
+		<?php
 		$attrs = '';
-		foreach( $options as $att => $value ){
-			if( $value ){
-				$attrs .= " {$att}=\"{$value}\" "; 
+		foreach ($options as $att => $value) {
+			if ($value) {
+				$attrs .= " {$att}=\"{$value}\" ";
 			}
 		}
 		?>
-		
-		<div class="g-recaptcha" <?php echo $attrs; ?> ></div>
+
+        <div class="g-recaptcha"  id="um-<?php _e( $args['form_id'] ); ?>" <?php echo $attrs; ?> ></div>
 		<?php
 
-		if ( UM()->form()->has_error( 'recaptcha' ) ) {
+		if (UM()->form()->has_error( 'recaptcha' )) {
 			echo '<div class="um-field-error">' . UM()->form()->errors['recaptcha'] . '</div>';
 		}
 
 	}
 
 	/***
-	***	@form error handling
-	***/
-	add_action('um_submit_form_errors_hook', 'um_recaptcha_validate', 20);
-	function um_recaptcha_validate( $args ){
-		if ( isset($args['mode']) && !in_array( $args['mode'], array('login','register') ) && ! isset( $args['_social_login_form'] ) ) return;
+	 ***    @form error handling
+	 ***/
+	add_action( 'um_submit_form_errors_hook', 'um_recaptcha_validate', 20 );
+	function um_recaptcha_validate( $args )
+	{
+		if (isset( $args['mode'] ) && !in_array( $args['mode'], array( 'login', 'register' ) ) && !isset( $args['_social_login_form'] )) return;
 
-		if ( ! UM()->reCAPTCHA_API()->captcha_allowed( $args ) ) return;
+		if (!UM()->reCAPTCHA_API()->captcha_allowed( $args )) return;
 
-		$your_secret = trim( um_get_option('g_recaptcha_secretkey') );
+		$your_secret = trim( um_get_option( 'g_recaptcha_secretkey' ) );
 		$client_captcha_response = $_POST['g-recaptcha-response'];
 		$user_ip = $_SERVER['REMOTE_ADDR'];
 
-		$response = wp_remote_get("https://www.google.com/recaptcha/api/siteverify?secret=$your_secret&response=$client_captcha_response&remoteip=$user_ip");
+		$response = wp_remote_get( "https://www.google.com/recaptcha/api/siteverify?secret=$your_secret&response=$client_captcha_response&remoteip=$user_ip" );
 
 		$error_codes = array(
-			'missing-input-secret' => __('The secret parameter is missing.','um-recaptcha'),
-			'invalid-input-secret' => __('The secret parameter is invalid or malformed.','um-recaptcha'),
-			'missing-input-response' => __('The response parameter is missing.','um-recaptcha'),
-			'invalid-input-response' => __('The response parameter is invalid or malformed.','um-recaptcha'),
+			'missing-input-secret'   => __( 'The secret parameter is missing.', 'um-recaptcha' ),
+			'invalid-input-secret'   => __( 'The secret parameter is invalid or malformed.', 'um-recaptcha' ),
+			'missing-input-response' => __( 'The response parameter is missing.', 'um-recaptcha' ),
+			'invalid-input-response' => __( 'The response parameter is invalid or malformed.', 'um-recaptcha' ),
 		);
 
-		
-		if( is_array( $response ) ) {
+
+		if (is_array( $response )) {
 
 			$result = json_decode( $response['body'] );
-			
-			if( isset( $result->{'error-codes'} ) && ! $result->success ){
-				foreach( $result->{'error-codes'} as $key => $error_code ){
 
-					if(  $error_code == 'missing-input-response' ){
+			if (isset( $result->{'error-codes'} ) && !$result->success) {
+				foreach ($result->{'error-codes'} as $key => $error_code) {
+
+					if ($error_code == 'missing-input-response') {
 						UM()->form()->add_error( 'recaptcha', __( 'Please confirm you are not a robot', 'um-recaptcha' ) );
-					}else{ 
-						UM()->form()->add_error( 'recaptcha', $error_codes[ $error_code ] );
+					} else {
+						UM()->form()->add_error( 'recaptcha', $error_codes[$error_code] );
 					}
 				}
 			}
 
-		} 
+		}
 
+	}
+
+	function um_recaptcha_google_onload()
+	{
+		if ('invisible' != um_get_option( 'g_recaptcha_size' )) {
+			?>
+            <script>
+                var onloadCallback = function () {
+                    jQuery('.g-recaptcha').each(function (i) {
+                        grecaptcha.render(jQuery(this).attr('id'), {
+                            'sitekey': jQuery(this).attr('data-sitekey'),
+                            'theme': jQuery(this).attr('data-theme')
+                        });
+
+                    });
+                }
+            </script>
+			<?php
+		}
 	}
--- includes/core/class-recaptcha-enqueue.php
+++ includes/core/class-recaptcha-enqueue.php
@@ -1,25 +1,41 @@
 <?php
-namespace um_ext\um_recaptcha\core;
+
+	namespace um_ext\um_recaptcha\core;
 
 
-class reCAPTCHA_Enqueue {
+	class reCAPTCHA_Enqueue
+	{
 
-	function __construct() {
+		function __construct()
+		{
 
-		add_action('wp_enqueue_scripts',  array(&$this, 'wp_enqueue_scripts'), 0);
+			add_action( 'wp_enqueue_scripts', array( &$this, 'wp_enqueue_scripts' ), 0 );
 
+		}
+
+		/***
+		 ***    @enqueue recaptcha
+		 ***/
+		function wp_enqueue_scripts()
+		{
+
+			wp_register_style( 'um_recaptcha', um_recaptcha_url . 'assets/css/um-recaptcha.css' );
+			wp_enqueue_style( 'um_recaptcha' );
+
+
+			$language_code = um_get_option( 'g_recaptcha_language_code' );
+			wp_enqueue_script( 'google-recapthca-api', "//www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit&hl=$language_code" );
+		}
+
+		public function add_defer_js_async( $tag )
+		{
+			$language_code = um_get_option( 'g_recaptcha_language_code' );
+			$defer_js_async = array( "//www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit&hl=$language_code" );
+			foreach ($defer_js_async as $script) {
+				if (TRUE == strpos( $tag, $script ))
+					return str_replace( ' src', ' async="async" defer="defer" src', $tag );
+			}
+
+			return $tag;
+		}
 	}
-
-	/***
-	***	@enqueue recaptcha
-	***/
-	function wp_enqueue_scripts(){
-
-		wp_register_style('um_recaptcha', um_recaptcha_url . 'assets/css/um-recaptcha.css' );
-		wp_enqueue_style('um_recaptcha');
-
-		
-
-	}
-
-}
