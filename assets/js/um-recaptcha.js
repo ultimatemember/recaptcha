ff4c11c3b5f3c363b5b3333ad2eb2860e89ee2fb
denisbaranov <dbaranov.work@gmail.com>
2020-02-16 18:59:26 +0200

    Fix error that appears on modal open/close

--- assets/js/um-recaptcha.js
+++ assets/js/um-recaptcha.js
@@ -1,11 +1,13 @@
 jQuery(document).ready(function () {
-	jQuery(document).on("um_messaging_open_login_form", function (e) {
-		um_recaptcha_refresh();
-	});
+	if (typeof (um_recaptcha_refresh) === 'function') {
+		jQuery(document).on("um_messaging_open_login_form", function (e) {
+			um_recaptcha_refresh();
+		});
 
-	jQuery(document).on("um_messaging_close_login_form", function (e) {
-		um_recaptcha_refresh();
-	});
+		jQuery(document).on("um_messaging_close_login_form", function (e) {
+			um_recaptcha_refresh();
+		});
+	}
 });
 
 /**
